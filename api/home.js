f58aa0b8720233539ad97074df5c710c35983d37
Indra <indra.b@skyshi.com>
2017-06-29 08:59:42 +0700

    refine code and add for filter

--- api/home.js
+++ api/home.js
@@ -5,15 +5,28 @@ function product (action) {
   let param = ''
   let check = [
     {value: action.page, string: 'page'},
-    {value: action.limit, string: 'limit'},
+    {value: action.size, string: 'size'},
     {value: action.category_id, string: 'category_id'},
-    {value: action.sort, string: 'sort'}
+    // sort is filled with 'newest', 'cheapest', 'expensive', 'selling'
+    {value: action.sort, string: 'sort'},
+    // price is filled with range, e.g. 500-1000
+    {value: action.price, string: 'price'},
+    // condition is filled with 'new' or 'used' or '' for both
+    {value: action.condition, string: 'condition'},
+    // other is filled with 'discount', 'verified', 'wholesaler'
+    {value: action.other, string: 'other'},
+    // brands is filled with number separated by comma if more than one, e.g. 2,5,3
+    {value: action.brands, string: 'brands'},
+    // services is same with brands
+    {value: action.services, string: 'services'},
+    // address is number of district_id
+    {value: action.address, string: 'address'}
   ]
 
   let indeksCheck = []
   let i = 0
   for (i = 0; i < check.length; i++) {
-    if (check[i].value !== undefined) {
+    if (check[i].value !== undefined || check[i].value !== '') {
       indeksCheck.push(i)
     }
   }
