571bae1831f43f6a8ef76489489eb3a9f44df96d
Indra <indra.b@skyshi.com>
2017-07-03 08:14:14 +0700

    add bad gateway error handling

--- config.js
+++ config.js
@@ -4,18 +4,28 @@ export const serviceUrl = 'https://private-f0902d-komuto.apiary-mock.com'
 export const apiKomuto = 'https://api.komuto.skyshi.com'
 
 export function errorHandling (actionType, res) {
-  const error = {
+  const errorOffline = {
     message: 'Your device is offline',
     code: 'ENOENT',
     isOnline: false
   }
+  const errorBadGateway = {
+    message: 'Bad Gateway',
+    code: 502,
+    isOnline: true
+  }
+
   const data = res.response
   if (data !== undefined) {
-    const {data} = res.response
-    data.isOnline = true
-    return put({ type: actionType, ...data })
+    if (data.status !== 502) {
+      const {data} = res.response
+      data.isOnline = true
+      return put({ type: actionType, ...data })
+    } else {
+      return put({ type: actionType, ...errorBadGateway })
+    }
   } else {
-    return put({ type: actionType, ...error })
+    return put({ type: actionType, ...errorOffline })
   }
 }
 
