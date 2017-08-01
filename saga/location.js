92337b80ad265b9cf0b36585f952dd88e5a208d0
syakuronabdan <abdan.s@students.amikom.ac.id>
2017-08-01 15:18:08 +0700

    Add query on get district

--- saga/location.js
+++ saga/location.js
@@ -3,6 +3,6 @@ import * as apis from '../api/location'
 import { buildSaga } from '../config'
 
 export const getProvince = buildSaga([], apis.getProvince, actions.GET_PROVINCE)
-export const getDistrict = buildSaga(['province_id'], apis.getDistrict, actions.GET_DISTRICT)
+export const getDistrict = buildSaga(['province_id', 'q'], apis.getDistrict, actions.GET_DISTRICT)
 export const getSubDistrict = buildSaga(['district_id'], apis.getSubDistrict, actions.GET_SUBDISTRICT)
 export const getVillage = buildSaga(['sub_district_id'], apis.getVillage, actions.GET_VILLAGE)
