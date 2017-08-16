0229f01ce8cf7b0e079d33edd2615eeb23131962
syakuronabdan <abdan.s@students.amikom.ac.id>
2017-08-16 08:18:04 +0700

    KOM-740: Update get store catalog products

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {store} from './store'
 // import * as location from './actions/location'
 // import * as product from './actions/product'
 // import * as review from './actions/review'
-// import * as stores from './actions/stores'
+import * as stores from './actions/stores'
 // import * as address from './actions/address'
 // import * as bank from './actions/bank'
 // import * as catalog from './actions/catalog'
@@ -15,12 +15,11 @@ import {store} from './store'
 // import * as transaction from './actions/transaction'
 // import {localStorage} from './localStorage'
 store.subscribe(function () {
-  console.log(JSON.stringify(store.getState().stores, null, 2))
+  console.log(JSON.stringify(store.getState().storeCatalogProducts, null, 2))
 })
 
 // TODO: Create JS DOC on every action page
 // TODO: Handle when get store products by catalog with no id
-// TODO: Delete choose payment
 
 // store.dispatch(user.register({name: 'Indra Bayu', phone_number: '081327', email: 'k.indrabayu@gmail.com', gender: 'L', password: 'aku'}))
 // setTimeout (function () {
@@ -219,7 +218,7 @@ store.subscribe(function () {
 // store.dispatch(expedition.getStoreExpeditions())
 // store.dispatch(bank.updateBankAccount({ id: 15, code: '10373', master_bank_id: 2, holder_name: 'aep', holder_account_number: '43254362541', bank_branch_office_name: 'iap' }))
 // store.dispatch(bank.deleteBankAccount({ id: 31, code: '10373' }))
-// store.dispatch(stores.getStoreCatalogProducts({ id: 33 }))
+store.dispatch(stores.getStoreCatalogProducts({ id: 33, hidden: true }))
 // store.dispatch(payment.getPaymentMethods())
 // store.dispatch(payment.choosePaymentMethods({ id: 259, payment_method_id: 1 }))
 // store.dispatch(payment.confirmTransfer({ id: 259, bank_account_id: 10, date: 1477242000, amount: 150000, bank: 'BNI', holder_account_number: 837375393910, attachment: 'attachment.png' }))
